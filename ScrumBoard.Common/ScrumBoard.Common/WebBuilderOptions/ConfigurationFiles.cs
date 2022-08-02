4c91f34d16b72cb406916a1470b73c6577248e85
Edu arevalo <eabigboy4@gmail.com>
2022-08-01 23:09:54 -0600

    Creacion de la estructura del proyecto

---
+++ ScrumBoard.Common/ScrumBoard.Common/WebBuilderOptions/ConfigurationFiles.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace ScrumBoard.Common.WebBuilderOptions
+{
+    public static class ConfigurationFiles
+    {
+        public static WebApplicationBuilder AddCommonConfiguration(this WebApplicationBuilder builder)
+        {
+            builder.Configuration.AddJsonFile("appsettings.local.json", true, true);
+            return builder;
+        }
+    }
+}
