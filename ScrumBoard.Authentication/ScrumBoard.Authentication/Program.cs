30f5f9d6c63c945569664f508e511a44620b1101
Edu arevalo <eabigboy4@gmail.com>
2022-08-09 21:00:56 -0600

    Identity configuration

--- ScrumBoard.Authentication/ScrumBoard.Authentication/Program.cs
+++ ScrumBoard.Authentication/ScrumBoard.Authentication/Program.cs
@@ -1,7 +1,84 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using OpenIddict.Abstractions;
+using Quartz;
+using ScrumBoard.Common.DatabaseContext;
+using ScrumBoard.Common.WebBuilderOptions;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.AddCommonConfiguration();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SecurityDbContext>(config =>
+{
+    config.UseSqlServer(configuration.GetConnectionString("SecurityDb"), x => x.MigrationsAssembly("ScrumBoard.Authentication"));
+    config.UseOpenIddict();
+});
+
+builder.Services
+    .AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<SecurityDbContext>();
+
+builder.Services.AddQuartz(options =>
+{
+    options.UseMicrosoftDependencyInjectionJobFactory();
+    options.UseSimpleTypeLoader();
+    options.UseInMemoryStore();
+});
+
+builder.Services.AddQuartzHostedService(options => options.WaitForJobsToComplete = true);
+
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.ClaimsIdentity.UserNameClaimType = OpenIddictConstants.Claims.Name;
+    options.ClaimsIdentity.UserIdClaimType = OpenIddictConstants.Claims.Subject;
+    options.ClaimsIdentity.RoleClaimType = OpenIddictConstants.Claims.Role;
+    options.ClaimsIdentity.EmailClaimType = OpenIddictConstants.Claims.Email;
+    options.ClaimsIdentity.SecurityStampClaimType = OpenIddictConstants.Claims.AuthenticationTime;
+});
+
+builder.Services.AddOpenIddict()
+    .AddCore(options =>
+    {
+        options.UseEntityFrameworkCore()
+            .UseDbContext<SecurityDbContext>();
+    })
+    .AddServer(options =>
+    {
+        options.SetAuthorizationEndpointUris("/connect/authorize")
+            .SetLogoutEndpointUris("/connect/logout")
+            .SetTokenEndpointUris("/connect/token")
+            .SetUserinfoEndpointUris("/connect/userinfo");
+
+        options.RegisterScopes(OpenIddictConstants.Scopes.Email, OpenIddictConstants.Scopes.Profile, OpenIddictConstants.Scopes.Roles);
+
+        options.AllowAuthorizationCodeFlow();
+        options.AllowRefreshTokenFlow();
+
+        options.UseReferenceAccessTokens();
+        options.UseReferenceRefreshTokens();
+
+        options.SetAccessTokenLifetime(TimeSpan.FromMinutes(30));
+        options.SetRefreshTokenLifetime(TimeSpan.FromDays(7));
+
+        options.AddDevelopmentEncryptionCertificate()
+            .AddDevelopmentSigningCertificate();
+
+        // Register ASP.NET Core host and configuration options
+        options.UseAspNetCore()
+            .EnableAuthorizationEndpointPassthrough()
+            .EnableTokenEndpointPassthrough();
+    })
+    .AddValidation(options =>
+    {
+        options.UseLocalServer();
+
+        // Register the ASP.NET Core host.
+        options.UseAspNetCore();
+    });
 
 var app = builder.Build();
 
@@ -18,10 +95,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
---
+++ ScrumBoard.Common/ScrumBoard.Common/DatabaseContext/SecurityDbContext.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ScrumBoard.Common.DatabaseContext
+{
+    public class SecurityDbContext : IdentityDbContext
+    {
+        public SecurityDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
